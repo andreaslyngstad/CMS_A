f2d62a16ec49a6436942f77ab176532db9acc630
andreaslyngstad <andreaslyngstad@gmail.com>
2009-09-24 14:52:40 +0200

    Second Commit - with deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,10 @@
-set :application, "grinakervev"
-set :repository,  "set your repository location here"
+set :application, "teatercamp.no"
+set :repository,  "."
+
+
+set :user, "teatercamp"
+set :deploy_to, "home/#{user}/#{application}"
+set :use_sudo, false
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
@@ -9,7 +14,22 @@ set :repository,  "set your repository location here"
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
 # set :scm, :subversion
+set :scm, :git
+set :deploy_via, :copy
+set :copy_remote_dir, "home/#{user}/"
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+role :app, application
+role :web, application
+role :db,  application, :primary => true
+
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+end
